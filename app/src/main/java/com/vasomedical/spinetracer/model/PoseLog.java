33389cc1817c085bc8e3c08f88c9140b076aa4fc
dehualai <dehua.lai@aicuretechnologies.com>
2017-09-25 22:48:47 -0400

    calibrate for position vs position detection

--- app/src/main/java/com/vasomedical/spinetracer/model/PoseLog.java
+++ app/src/main/java/com/vasomedical/spinetracer/model/PoseLog.java
@@ -25,13 +25,40 @@ public class PoseLog {
         poseList = new ArrayList<Pose>();
     }
 
-    public void recordPoseData(TangoPoseData poseData){
+    public void recordPoseData(TangoPoseData poseData, TangoPoseData initPose){
         float translation[] = poseData.getTranslationAsFloats();
         float orientation[] = poseData.getRotationAsFloats();
-        Pose pose = new Pose(translation[0], translation[1], translation[2],
-                orientation[0], orientation[1], orientation[2], orientation[3]
-                );
-        poseList.add(pose);
+
+
+        if (initPose!=null){
+            float translationInit[] = initPose.getTranslationAsFloats();
+            float orientationInit[] = initPose.getRotationAsFloats();
+
+            Pose pose = new Pose(
+                    translation[0] - translationInit[0],
+                    translation[1] - translationInit[1],
+                    translation[2] - translationInit[2],
+                    orientation[0] - orientationInit[0],
+                    orientation[1] - orientationInit[1],
+                    orientation[2] - orientationInit[2],
+                    orientation[3] - orientationInit[3]
+            );
+            poseList.add(pose);
+        }else{
+            Pose pose = new Pose(
+                    translation[0] ,
+                    translation[1] ,
+                    translation[2] ,
+                    orientation[0] ,
+                    orientation[1] ,
+                    orientation[2] ,
+                    orientation[3]
+            );
+            poseList.add(pose);
+
+        }
+
+
     }
 
 
