11fce836cbc83eca60cc8ca8762c93b86ffa98d0
Quan <weiyideai520@foxmail.com>
2018-07-16 10:10:42 +0800

    医生注册时，医院和科室是选择的

--- app/src/main/java/com/vasomedical/spinetracer/activity/RegisterActivity.java
+++ app/src/main/java/com/vasomedical/spinetracer/activity/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.vasomedical.spinetracer.activity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -14,21 +16,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.vasomedical.spinetracer.R;
+import com.vasomedical.spinetracer.activity.presenter.CompanyPresenter;
+import com.vasomedical.spinetracer.activity.presenter.CompanyPresenterCompl;
 import com.vasomedical.spinetracer.activity.presenter.DoctorPresenter;
 import com.vasomedical.spinetracer.activity.presenter.DoctorPresenterCompl;
+import com.vasomedical.spinetracer.activity.view.CompanyView;
 import com.vasomedical.spinetracer.activity.view.DoctorView;
+import com.vasomedical.spinetracer.model.CompanyClassModel;
+import com.vasomedical.spinetracer.model.CompanyModel;
 import com.vasomedical.spinetracer.model.DoctorModel;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 //界面--用户注册
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, DoctorView {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener,
+        CompoundButton.OnCheckedChangeListener, DoctorView, CompanyView {
     private EditText editName, editPass, editPass2, editRealName, editEmail, editPhone;
-    private EditText editCompany, editClass;
+    private TextView editCompany, editClass;
     private View buttonBack, buttonRegister, buttonCompanyMore, buttonClassMore;
     private CheckBox buttonEye, buttonEye2;
     private DoctorPresenter doctorPresenter;
+    private CompanyPresenter companyPresenter;
+    private CompanyModel companyModel;
 
 
     @Override
@@ -50,16 +60,19 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         editRealName = (EditText) findViewById(R.id.editRealName);
         editEmail = (EditText) findViewById(R.id.editEmail);
         editPhone = (EditText) findViewById(R.id.editPhone);
-        editCompany = (EditText) findViewById(R.id.editCompany);
-        editClass = (EditText) findViewById(R.id.editClass);
+        editCompany = (TextView) findViewById(R.id.editCompany);
+        editClass = (TextView) findViewById(R.id.editClass);
         buttonEye = (CheckBox) findViewById(R.id.buttonEye);
         buttonEye2 = (CheckBox) findViewById(R.id.buttonEye2);
     }
 
     private void addAction() {
         doctorPresenter = new DoctorPresenterCompl(this, this);
+        companyPresenter = new CompanyPresenterCompl(this, this);
+        companyPresenter.reqCompanyInfo();
         buttonRegister.setOnClickListener(this);
         buttonBack.setOnClickListener(this);
+        editClass.setOnClickListener(this);
         buttonEye.setOnCheckedChangeListener(this);
         buttonEye2.setOnCheckedChangeListener(this);
 
@@ -86,6 +99,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             userRegister();
         } else if (v == buttonBack) {
             onBackPressed();
+        } else if (v == editClass) {
+            ShowClassChoise();
         }
     }
 
@@ -136,11 +151,55 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         } else if (TextUtils.isEmpty(editPhone.getText())) {
             Toast.makeText(this, "请输入手机号码", Toast.LENGTH_SHORT).show();
         } else if (TextUtils.isEmpty(editCompany.getText())) {
-            Toast.makeText(this, "请输入医院", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "请在医院管理里面添加医院名称", Toast.LENGTH_SHORT).show();
         } else if (TextUtils.isEmpty(editClass.getText())) {
-            Toast.makeText(this, "请输入科室空", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "请选择医院科室", Toast.LENGTH_SHORT).show();
         } else {
             doctorPresenter.register(doctorModel);
         }
+    }
+
+    private void ShowClassChoise() {
+        if (companyModel != null && companyModel.getClassModelList() != null && companyModel.getClassModelList().size() > 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this, android.R.style.Theme_Holo_Light_Dialog);
+            //builder.setIcon(R.drawable.ic_launcher);
+            builder.setTitle("选择科室");
+            //    指定下拉列表的显示数据
+            final String[] className = new String[companyModel.getClassModelList().size()];
+            for (int i = 0; i < className.length; i++) {
+                className[i] = companyModel.getClassModelList().get(i).getName();
+            }
+            //    设置一个下拉的列表选择项
+            builder.setItems(className, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    editClass.setText(className[which]);
+                }
+            });
+            builder.show();
+        } else {
+            Toast.makeText(this, "请在医院管理里面添加科室", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void updateCompanyUI(CompanyModel companyModel) {
+        this.companyModel = companyModel;
+        if (companyModel != null) {
+            editCompany.setText(companyModel.getName());
+            if (companyModel.getClassModelList() != null && companyModel.getClassModelList().size() > 0) {
+                editClass.setText(companyModel.getClassModelList().get(0).getName());
+            }
+        }
+    }
+
+    @Override
+    public void saveCompayInfoCallBack(boolean success, String msg) {
+
+    }
+
+    @Override
+    public void saveCompayClassInfoCallBack(boolean success, String msg) {
+
     }
 }
